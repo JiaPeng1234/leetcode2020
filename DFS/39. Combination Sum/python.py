5ba494de147820b4c5ddc469740aef417fd1b3b8
PJ <jiape123456@gmail.com>
2020-08-09 12:57:02 +0200

    Create python.py
    python approach

---
+++ DFS/39. Combination Sum/python.py
@@ -0,0 +1,15 @@
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        ans = []
+        candidates.sort()
+        self.dfs(candidates,target,0,[],ans)
+        return ans
+    
+    def dfs(self,candidates,target,s,cur,ans):
+        if target==0:
+            ans.append(cur)
+            return
+        for i in range(s,len(candidates)):
+            if candidates[i]>target:
+                break
+            self.dfs(candidates,target-candidates[i],i,cur+[candidates[i]],ans)
