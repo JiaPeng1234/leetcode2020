bc973c9b8678878f896dbf35fa185f366b32d9df
PJ <jiape123456@gmail.com>
2020-08-10 15:51:39 +0200

    Update Solution.py

--- DFS/40. Combination Sum II/Solution.py
+++ DFS/40. Combination Sum II/Solution.py
@@ -1,3 +1,6 @@
+# from jiapeng
+# Runtime: 32 ms, faster than 99.80% of Python3 online submissions for Combination Sum II.
+# Memory Usage: 13.8 MB, less than 82.27% of Python3 online submissions for Combination Sum II.
 class Solution:
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
         ans = []
@@ -12,6 +15,7 @@ class Solution:
         for i in range(starter, len(candidates)):
             if candidates[i]>target:
                 break
+            # need to ensure when i equals i-1, then it must be the first element that being tranversed in the current loop     
             if i!=starter and candidates[i]==candidates[i-1]:
                 continue
             self.dfs(target-candidates[i], candidates, cur+[candidates[i]], ans, i+1)
