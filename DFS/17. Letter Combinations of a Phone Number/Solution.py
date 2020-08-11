e6697f3c141e21f0af03629850989880c00cb369
PJ <jiape123456@gmail.com>
2020-08-11 22:23:40 +0200

    Update Solution.py

--- DFS/17. Letter Combinations of a Phone Number/Solution.py
+++ DFS/17. Letter Combinations of a Phone Number/Solution.py
@@ -1,7 +1,7 @@
-By jiapeng
-using general dfs framework :)  dfs framework just OP 
-Runtime: 28 ms, faster than 84.31% of Python3 online submissions for Letter Combinations of a Phone Number.
-Memory Usage: 13.6 MB, less than 96.94% of Python3 online submissions for Letter Combinations of a Phone Number.
+# By jiapeng
+# using general dfs framework :)  dfs framework just OP 
+# Runtime: 28 ms, faster than 84.31% of Python3 online submissions for Letter Combinations of a Phone Number.
+# Memory Usage: 13.6 MB, less than 96.94% of Python3 online submissions for Letter Combinations of a Phone Number.
 
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
@@ -34,3 +34,31 @@ class Solution:
                 self.dfs(mapp, digits, ans, cur, i+1)
                 cur = cur[:-1]
                 
+                
+# By jiapeng
+# using core idea of dynamic programming: every time we add a new bit, just combine it with all the availble element in current output array
+# Runtime: 20 ms, faster than 99.02% of Python3 online submissions for Letter Combinations of a Phone Number.
+# Memory Usage: 13.9 MB, less than 36.02% of Python3 online submissions for Letter Combinations of a Phone Number.
+
+class Solution2:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if not len(digits):
+            return []
+        mobilemap={'0':[" "],
+             '1':[],
+             '2':['a','b','c'],
+             '3':['d','e','f'],
+             '4':['g','h','i'],
+             '5':['j','k','l'],
+             '6':['m','n','o'],
+             '7':['p','q','r','s'],
+             '8':['t','u','v'],
+             '9':['w','x','y','z']}
+        ans=[""]    # important, "" needs to be here
+        for digit in digits:
+            tmp=[]
+            for digit_letter in mobilemap[digit]:
+                for i in ans:
+                    tmp.append(i+digit_letter)
+            ans=tmp
+        return ans
