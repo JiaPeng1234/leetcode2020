de6f230b03dcabdfcec8f6f20406983620ef8236
PJ <jiape123456@gmail.com>
2020-08-12 16:45:31 +0200

    Update Solution.java

--- DFS/77. Combinations/Solution.java
+++ DFS/77. Combinations/Solution.java
@@ -1,1 +1,31 @@
 // https://leetcode.com/problems/combinations/
+
+/**
+By jiapeng
+Runtime: 1 ms, faster than 100.00% of Java online submissions for Combinations.
+Memory Usage: 40.8 MB, less than 76.27% of Java online submissions for Combinations.
+**/
+
+class Solution {
+    public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> ans = new ArrayList();
+        if (k > n)
+            return ans;
+        dfs(0, k, n, new ArrayList(), ans, 1);
+        return ans;
+    }
+    
+    private void dfs(int d, int k, int n, List<Integer>cur, List<List<Integer>>ans, int starter){
+        if(d == k){
+            ans.add(new ArrayList(cur));
+            return;
+        }
+        for(int i = starter; i < n+1; i++){
+            if(n+1-i < k-d)
+                break;
+            cur.add(i);
+            dfs(d+1, k, n, cur, ans, i+1);
+            cur.remove(cur.size()-1);
+        }
+    }
+}
