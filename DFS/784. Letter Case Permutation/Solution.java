e7d112e131910329095edcadc2d5baf6d8fc70c0
PJ <jiape123456@gmail.com>
2020-08-14 01:09:03 +0200

    Update Solution.java

--- DFS/784. Letter Case Permutation/Solution.java
+++ DFS/784. Letter Case Permutation/Solution.java
@@ -5,7 +5,6 @@ By Jiapeng
 Runtime: 8 ms, faster than 37.05% of Java online submissions for Letter Case Permutation.
 Memory Usage: 40.5 MB, less than 61.32% of Java online submissions for Letter Case Permutation.
 **/
-
 class Solution {
     public List<String> letterCasePermutation(String S) {
         List<String> ans = new ArrayList(); 
@@ -30,6 +29,48 @@ class Solution {
                 }
             } else{
                 dfs(d+1, S, ans, cur+bit, i+1);
+            }
+        }
+    }
+}
+
+/**
+By Jiapeng
+optimized method 
+Runtime: 1 ms, faster than 100.00% of Java online submissions for Letter Case Permutation.
+Memory Usage: 40.6 MB, less than 51.76% of Java online submissions for Letter Case Permutation.
+**/
+class Solution2 {
+    public List<String> letterCasePermutation(String S) {
+        List<String> ans = new ArrayList(); 
+        // String本质上是字符数组，所以此处把S转换为char[]遍历更为节省时间
+        // optimize here: use char[] array instead of String, cause essentially the Class String is a char array
+        dfs(0, S, ans, S.toCharArray(), 0);
+        return ans;
+    }
+    
+    private void dfs(int d, String S, List<String> ans, char[] cur, int starter){
+        if(d == S.length()){
+            ans.add(new String(cur));
+            return;
+        }
+        for(int i=starter; i<S.length(); i++){
+            if(i != starter)
+                break;
+            char bit = S.charAt(i);
+            // if current bit is a letter
+            // then this bit is either in upper- or lowercase
+            // optimize here, no need to use for-loop and backtrack
+            // just assign lowercase and uppercase letter one by one
+            // char[] array is static
+            if(Character.isLetter(bit)){    
+                cur[i] = Character.toLowerCase(bit);
+                dfs(d+1, S, ans, cur, i+1);
+                cur[i] = Character.toUpperCase(bit);
+                dfs(d+1, S, ans, cur, i+1);
+            } else{
+                cur[i] = bit;
+                dfs(d+1, S, ans, cur, i+1);
             }
         }
     }
