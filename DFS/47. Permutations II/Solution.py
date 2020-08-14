e46974191e0034b55cfe7b9011f42fb733ecb4f0
PJ <jiape123456@gmail.com>
2020-08-14 15:33:09 +0200

    Update Solution.py

--- DFS/47. Permutations II/Solution.py
+++ DFS/47. Permutations II/Solution.py
@@ -3,13 +3,13 @@
 # By Jiapeng
 # Runtime: 64 ms, faster than 63.07% of Python3 online submissions for Permutations II.
 # Memory Usage: 14 MB, less than 52.23% of Python3 online submissions for Permutations II.
-
+# DFS框架参考3：此方法可以作为DFS参考框架
 class Solution:
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
         ans = []
         mask = [0] * len(nums)
-        self.dfs(0, nums, ans, [], mask)
+        self.dfs(0, nums, ans, [], mask)    # 注意点1：不使用starter，而是使用一个相等大小的mask数组来标记该元素用过没有
         return ans
         
     def dfs(self, d, nums, ans, cur, mask):
@@ -17,6 +17,8 @@ class Solution:
             ans.append(cur)
             return
         for i in range(0, len(nums)):
+            # 注意点2：剪枝条件continue
+            # 和 https://github.com/XincredibleY/leetcode2020/blob/master/DFS/40.%20Combination%20Sum%20II/Solution.py 中的continue剪枝条件相对比
             if i > 0 and nums[i] == nums[i-1] and mask[i-1] == 0:
                 continue
             if mask[i] == 1:
