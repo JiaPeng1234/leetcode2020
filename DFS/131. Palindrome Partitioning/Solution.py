e5bc26bf1cd823381c96fd44751b73a3cc2a4751
PJ <jiape123456@gmail.com>
2020-08-22 17:21:35 +0200

    Update Solution.py

--- DFS/131. Palindrome Partitioning/Solution.py
+++ DFS/131. Palindrome Partitioning/Solution.py
@@ -1,4 +1,28 @@
 # https://leetcode.com/problems/palindrome-partitioning/
 
 # By Jiapeng
-#
+# DFS partition problem
+# Runtime: 92 ms, faster than 56.86% of Python3 online submissions for Palindrome Partitioning.
+# Memory Usage: 14.3 MB, less than 20.58% of Python3 online submissions for Palindrome Partitioning.
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        ans = []
+        self.dfs(ans, [], 0, s)
+        return ans
+        
+    def dfs(self, ans, cur, starter, s):
+        if starter == len(s):
+            ans.append(cur)
+            return
+        for i in range(starter, len(s)):
+            if not self.isPalindrome(s, starter, i):
+                continue
+            self.dfs(ans, cur + [s[starter:i+1]], i + 1, s)
+    
+    def isPalindrome(self, s, left, right):
+        while left < right:
+            if s[left] != s[right]:
+                return False
+            left += 1
+            right -= 1
+        return True
