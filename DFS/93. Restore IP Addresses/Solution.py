27f90be4b6eca6e5e8562d570b2d7203b3d7ae78
PJ <jiape123456@gmail.com>
2020-08-22 19:00:06 +0200

    Update Solution.py

--- DFS/93. Restore IP Addresses/Solution.py
+++ DFS/93. Restore IP Addresses/Solution.py
@@ -33,3 +33,29 @@ class Solution:
                 self.dfs(s, count + 1, ans, cur + s[i:i+2] + '.', i + 2)
                 if i + 3 <= len(s) and s[i:i+3] <= "255":   # for 3-digits IP
                     self.dfs(s, count + 1, ans, cur + s[i:i+3] + '.', i + 3)
+
+# By Jiapeng
+# DFS partition problem 使用DFS-partition优化过的代码，为了美观把2-digits和3-digits的情况合并计算，多了一个<=255的判断，会略微拖慢运算时间，
+# 但是DFS-partition框架是很可取的，比起Solution1的代码更容易记住和理解
+# Runtime: 52 ms, faster than 29.22% of Python3 online submissions for Restore IP Addresses.
+# Memory Usage: 14 MB, less than 19.85% of Python3 online submissions for Restore IP Addresses.
+# DFS框架参考5：此方法可以作为DFS参考框架
+class Solution2:
+    def restoreIpAddresses(self, s: str) -> List[str]:
+        ans = []
+        self.dfs(s, 0, ans, "", 0)
+        return ans
+    
+    def dfs(self, s, count, ans, cur, starter):
+        if count == 4:
+            if starter == len(s):
+                ans.append(cur[:-1])
+            return
+        if starter >= len(s):
+            return
+        for end in range(starter, starter + 3):
+            if end < len(s):
+                if end == starter:      # 1-digit IP
+                    self.dfs(s, count + 1, ans, cur + s[end] + '.', end + 1)
+                elif s[starter] != '0' and int(s[starter:end+1]) <= int("255"):      # for 2-digits and 3-dogits IP
+                    self.dfs(s, count + 1, ans, cur + s[starter:end+1] + '.', end + 1)                    
