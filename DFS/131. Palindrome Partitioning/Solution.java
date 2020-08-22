8c7fec16b96ec5e63344019190e9d0ddc3205fab
PJ <jiape123456@gmail.com>
2020-08-22 18:12:40 +0200

    Update Solution.java

--- DFS/131. Palindrome Partitioning/Solution.java
+++ DFS/131. Palindrome Partitioning/Solution.java
@@ -2,4 +2,36 @@
 
 /**
 By Jiapeng
+DFS partition problem
+Runtime: 1 ms, faster than 100.00% of Java online submissions for Palindrome Partitioning.
+Memory Usage: 40.4 MB, less than 69.48% of Java online submissions for Palindrome Partitioning.
 **/
+class Solution {
+    public List<List<String>> partition(String s) {
+        List<List<String>> ans = new ArrayList<>();
+        dfs(s, 0, ans, new ArrayList<String>());
+        return ans;
+    }
+    
+    private void dfs(String s, int starter, List<List<String>> ans, List<String> cur){
+        if(starter == s.length()){
+            ans.add(new ArrayList<>(cur));
+            return;
+        }
+        for(int i = starter; i < s.length(); i++){
+            if(!isPalindrome(s, starter, i))
+                continue;
+            cur.add(s.substring(starter, i + 1));
+            dfs(s, i + 1, ans, cur);
+            cur.remove(cur.size() - 1);
+        }
+    }
+    
+    private boolean isPalindrome(String s, int left, int right){
+        while(left < right){
+            if(s.charAt(left++) != s.charAt(right--))
+               return false;
+        }
+        return true;
+    }
+}
