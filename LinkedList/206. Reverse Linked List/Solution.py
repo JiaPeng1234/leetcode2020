c0e11df01ad39bd5377469b4ac1f696215ac7c11
PJ <jiape123456@gmail.com>
2020-10-10 13:36:50 +0200

    Update Solution.py

--- LinkedList/206. Reverse Linked List/Solution.py
+++ LinkedList/206. Reverse Linked List/Solution.py
@@ -1,6 +1,26 @@
 # https://leetcode.com/problems/reverse-linked-list/
 
 # By Jiapeng
-# DFS partition problem
-# Runtime: 92 ms, faster than 56.86% of Python3 online submissions for Palindrome Partitioning.
-# Memory Usage: 14.3 MB, less than 20.58% of Python3 online submissions for Palindrome Partitioning.
+# linked list
+# Runtime: 32 ms, faster than 88.83% of Python3 online submissions for Reverse Linked List.
+# Memory Usage: 15.4 MB, less than 7.80% of Python3 online submissions for Reverse Linked List.
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        if not head or not head.next:
+            return head
+        A0 = None
+        A1 = head
+        A2 = head.next
+        while A1:
+            A1.next = A0
+            A0 = A1
+            A1 = A2
+            if A2:
+                A2 = A2.next
+        return A0
