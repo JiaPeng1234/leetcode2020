d92919bb70a504b1b2af47213d2f3c3c9ebddec9
PJ <jiape123456@gmail.com>
2020-10-10 13:11:25 +0200

    Update and rename List/新建文本文档.txt to LinkedList/206. Reverse Linked List/Solution.py

---
+++ LinkedList/206. Reverse Linked List/Solution.py
@@ -0,0 +1,6 @@
+# https://leetcode.com/problems/reverse-linked-list/
+
+# By Jiapeng
+# DFS partition problem
+# Runtime: 92 ms, faster than 56.86% of Python3 online submissions for Palindrome Partitioning.
+# Memory Usage: 14.3 MB, less than 20.58% of Python3 online submissions for Palindrome Partitioning.
